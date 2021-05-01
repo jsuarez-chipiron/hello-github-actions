c781d39a2d065462cbd4ec98530b7df92c2797c8
Javier Suarez <jsuarez@salesforce.com>
2021-05-01 21:02:19 +0200

    show sfdx version

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo "Hello world my name is $INPUT_MY_NAME"
-/work/sfdx/bin/sfdx plugins:install -f sfdx-xy-plugin
-/work/sfdx/bin/sfdx xy:auth:username:login -u jsuarez-xsgq@force.com -p JfP2hVh89SNY -a default
-/work/sfdx/bin/sfdx force:org:list > caca.txt
+/work/sfdx/bin/sfdx --version > caca.txt
 cat caca.txt
