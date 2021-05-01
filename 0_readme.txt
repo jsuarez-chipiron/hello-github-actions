043a1c7f0afe67a249e794b6d1b169ed363bd805
Javier Suarez <jsuarez@salesforce.com>
2021-05-01 20:45:46 +0200

    preparing clara

---
+++ 0_readme.txt
@@ -0,0 +1,1 @@
+http://salesforcexytools.com/2020/01/26/sfdx-xy-plugin/
--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Hello world my name is $INPUT_MY_NAME"
-/work/sfdx/bin/sfdx --version > caca.txt
+/work/sfdx/bin/sfdx force:org:list > caca.txt
 cat caca.txt
