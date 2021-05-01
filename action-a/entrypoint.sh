9a4d620a14412963c7e50643e0679ba8aedbbed3
Javier Suarez <jsuarez@salesforce.com>
2021-05-01 20:55:56 +0200

    auth to sfdc

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Hello world my name is $INPUT_MY_NAME"
+/work/sfdx/bin/sfdx xy:auth:username:login -u jsuarez-xsgq@force.com -p JfP2hVh89SNY -a default
 /work/sfdx/bin/sfdx force:org:list > caca.txt
 cat caca.txt
